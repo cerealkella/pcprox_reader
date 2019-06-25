029d26a427f1d2b35a10c61f20599d9a18086d4f
kellerjustin <kellerjustin@gmail.com>
2019-06-25 16:15:07 -0500

    version 0.3

--- pcprox_reader/cli.py
+++ pcprox_reader/cli.py
@@ -3,19 +3,18 @@
 """Console script for pcprox_reader."""
 import sys
 import click
-from .pcprox_reader import CardReader
+from .cardreader import CardReader
 
 
 @click.command()
 def main(args=None):
     """Console script for pcprox_reader."""
-    click.echo("Replace this message by putting your code into "
-               "pcprox_reader.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    click.echo(" Swipe a badge on the pcProx Reader ")
+    click.echo(" <<< Ctrl+C to quit >>> ")
+    reader = CardReader()
+    reader.swipe_loop()
     return 0
 
 
 if __name__ == "__main__":
-    reader = CardReader()
-    reader.swipe_loop()
     sys.exit(main())  # pragma: no cover
